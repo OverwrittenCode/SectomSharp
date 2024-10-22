57f1603148bc907c4ea6a8110c9749b24573eac0
OverwrittenCode <kylethapar2007@outlook.com>
2024-10-22 23:35:05 +0100

    feat(AuditLogChannel): set OperationType nullable

--- SectomSharp.Data/Models/AuditLogChannel.cs
+++ SectomSharp.Data/Models/AuditLogChannel.cs
@@ -7,5 +7,5 @@ public sealed class AuditLogChannel : Snowflake
     public required string WebhookUrl { get; set; }
 
     public AuditLogType AuditLogType { get; set; }
-    public OperationType OperationType { get; set; }
+    public OperationType? OperationType { get; set; }
 }
