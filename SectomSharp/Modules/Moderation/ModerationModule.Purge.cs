4084475b31f7af4771abe41a1fed442024ab74ef
OverwrittenCode <kylethapar2007@outlook.com>
2025-07-14 15:16:27 +0100

    perf(purge): replace FlattenAsync with LINQ chain
    - FlattenAsync internally materializes pages via ToArrayAsync before applying any filters or projections.
      https://github.com/discord-net/Discord.Net/blob/3fd06a350438313c34b66099e0943ca40493cebe/src/Discord.Net.Core/Extensions/AsyncEnumerableExtensions.cs#L16
    
    - This introduces unnecessary memory allocations and breaks deferred execution in the LINQ pipeline.
    - Instead, directly using SelectMany enables proper streaming and avoids intermediate buffering.
    - Improves both performance and memory efficiency during message fetches.

--- SectomSharp/Modules/Moderation/ModerationModule.Purge.cs
+++ SectomSharp/Modules/Moderation/ModerationModule.Purge.cs
@@ -20,17 +20,19 @@ public sealed partial class ModerationModule
         DateTimeOffset earliestAllowedPurgeDateTime = DateTimeOffset.UtcNow.AddDays(-14);
         var channel = (SocketTextChannel)Context.Channel;
 
-        List<IMessage> messages = (await channel.GetMessagesAsync(amount + 1).FlattenAsync())
-                                 .Where(message => message.Id != originalMessage.Id && message.CreatedAt >= earliestAllowedPurgeDateTime)
-                                 .ToList();
-
-        if (messages.Count == 0)
+        ulong[] messageIds = await channel.GetMessagesAsync(amount + 1)
+                                          .SelectMany(page => page.Where(message => message.Id != originalMessage.Id && message.CreatedAt >= earliestAllowedPurgeDateTime)
+                                                                  .Select(message => message.Id)
+                                                                  .ToAsyncEnumerable()
+                                           )
+                                          .ToArrayAsync();
+        if (messageIds.Length == 0)
         {
             await FollowupAsync("No messages newer than 2 weeks were found.");
             return;
         }
 
-        await channel.DeleteMessagesAsync(messages, DiscordUtils.GetAuditReasonRequestOptions(Context, reason));
+        await channel.DeleteMessagesAsync(messageIds, DiscordUtils.GetAuditReasonRequestOptions(Context, reason));
         await CaseUtils.LogAsync(DbContextFactory, Context, BotLogType.Purge, OperationType.Create, channelId: channel.Id, reason: reason);
     }
 }
