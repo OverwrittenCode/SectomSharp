b6f89f7411432939244a59bf71be78b8e53057a0
OverwrittenCode <kylethapar2007@outlook.com>
2025-07-21 14:38:54 +0100

    refactor: mark as UsedImplicitly

--- SectomSharp.Data/Entities/LevelingConfiguration.cs
+++ SectomSharp.Data/Entities/LevelingConfiguration.cs
@@ -4,7 +4,7 @@ namespace SectomSharp.Data.Entities;
 
 public sealed class LevelingConfiguration : BaseConfiguration
 {
-    public bool AccumulateMultipliers { get; set; }
+    public bool AccumulateMultipliers { get; [UsedImplicitly] set; }
     public double GlobalMultiplier { get; [UsedImplicitly(Reason = Constants.ValueGeneratedOnAdd)] set; }
     public uint GlobalCooldown { get; [UsedImplicitly(Reason = Constants.ValueGeneratedOnAdd)] set; }
 }
